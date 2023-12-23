167dfddb77bad5f50ae472b0a35635630f6043aa
KukVova <152005567+KukVova@users.noreply.github.com>
2023-12-23 12:46:38 +0200

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,27 +1,30 @@
 class Iterator():
     def __init__(self, max_num):
-        self.i = -2
+        self.i = 0
         self.max_num = max_num
     def __iter__(self):
-        self.i = -2
+        self.i = 0
         return self
+
     def __next__(self):
-        def gen():
-            i=0
-            while i!= 10:
-                yield i
-        generator = gen()
-        if self.i > self.max_num:
+        self.i += 1
+        if self.i == self.max_num:
             raise StopIteration
-        return generator
-iterator = Iterator(10)
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
-print(iterator.__next__())
+        def gen():
+            n = 0
+            while n != 10:
+                n += 2
+                yield n
+
+        return gen
+
+i = Iterator(5)
+
+g = i.__next__()
+f = g()
+print(next(f))
+print(next(f))
+print(next(f))
+print(next(f))
+print(next(f))
+
